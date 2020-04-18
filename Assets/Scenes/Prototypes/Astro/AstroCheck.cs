e4f23413f68b99e8f18d752c3a98a77a07c359c0
ToxicMask <tomas.carciofi@gmail.com>
2020-04-18 04:55:05 -0300

    Added Main Ship + Conditions of Winning

---
+++ Assets/Scenes/Prototypes/Astro/AstroCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroCheck : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("!");
+            // Temp -> Get Collision trough circle
+            Collider2D[] collisionArray = Physics2D.OverlapCircleAll(transform.position, 2.5f);
+
+            // Check all collisions
+            foreach (Collider2D collider in collisionArray)
+            {
+                IPlayerCanCheck obj = collider.gameObject.GetComponent<IPlayerCanCheck>();
+
+                if (obj != null) { obj.Check(gameObject);}
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Prototypes/SpaceShip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceShip : MonoBehaviour, IPlayerCanCheck
+{
+
+    [SerializeField] int minChipNumber = 5;
+
+    public void Check(GameObject player)
+    {
+        Debug.Log("Checked");
+        CheckPlayerProgress(player);
+    }
+
+    public void CheckPlayerProgress(GameObject player)
+    {
+        // Check for Astro Inventory
+        AstroInv astroInv = player.GetComponent<AstroInv>();
+
+        //Check Inv, if not null
+        if (astroInv != null)
+        {
+            // Check Victory
+            if (astroInv.currentShipChips >= minChipNumber) PlayerVictory();
+            
+        }
+
+    }
+
+   protected void PlayerVictory()
+    {
+        Debug.Log("Victory");
+        Application.Quit();
+    }
+
+}
