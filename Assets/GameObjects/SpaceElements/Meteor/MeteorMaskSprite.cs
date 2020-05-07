e4033f78ea3ec671cac886d4de3d11fcd03299af
ToxicMask <tomas.carciofi@gmail.com>
2020-05-07 01:03:57 -0300

    Meteor & Variants + Mask Iteraction on Editor

---
+++ Assets/GameObjects/SpaceElements/Meteor/MeteorMaskSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMaskSprite : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Config spritemask
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite != null) sprite.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+    }
+}
