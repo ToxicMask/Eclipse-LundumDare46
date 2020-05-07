e1b56855bab5cbc0e3c84c804beb4d79da9fef73
ToxicMask <tomas.carciofi@gmail.com>
2020-05-07 01:10:14 -0300

    Runtime Sprite Mask

--- Assets/GameObjects/SpaceElements/Meteor/RuntimeMaskSprite.cs
+++ Assets/GameObjects/SpaceElements/Meteor/RuntimeMaskSprite.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeteorMaskSprite : MonoBehaviour
+public class RuntimeMaskSprite : MonoBehaviour
 {
     private void Awake()
     {
