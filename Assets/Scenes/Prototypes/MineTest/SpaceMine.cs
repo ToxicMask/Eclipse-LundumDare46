eca9c4dcaf7dbaebf82417e75ae46c7644287705
ToxicMask <tomas.carciofi@gmail.com>
2020-05-21 04:33:56 -0300

    Space mine (game object)

---
+++ Assets/Scenes/Prototypes/MineTest/SpaceMine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMine : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log("!");
+
+        IExplosionContact affectedBody = collision.gameObject.GetComponent<IExplosionContact>();
+
+        if (affectedBody != null)
+        {
+            affectedBody.ExplosionAffected();
+        }
+
+        Destroy(gameObject);
+    }
+}
